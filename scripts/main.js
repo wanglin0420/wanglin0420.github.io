34b8fabd1a1b5fb4de104225bd715b00d506f15a
wangling <wl_0420@163.com>
2017-07-20 11:24:59 +0800

    html5 段落

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,19 @@
 var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Hello world!';
-a="bbbbbbb"
-
-function multiply(num1,num2) {
+var title = "我的名字是："
+function multiply(num1, num2) {
   var result = num1 * num2;
   return result;
 }
 
 var myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.jpg') {
-      myImage.setAttribute ('src','images/firefox2.jpg');
-    } else {
-      myImage.setAttribute ('src','images/firefox-icon.jpg');
-    }
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox-icon.jpg') {
+    myImage.setAttribute('src', 'images/firefox2.jpg');
+  } else {
+    myImage.setAttribute('src', 'images/firefox-icon.jpg');
+  }
 }
 
 var myButton = document.querySelector('button');
@@ -23,12 +21,12 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('输入你的名字.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = title + myName;
 }
-if(!localStorage.getItem('name')) {
+if (!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = title + storedName;
 }
-myButton.onclick=setUserName
+myButton.onclick = setUserName
